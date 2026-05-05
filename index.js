f5b104523cc317a2d91b3d9ca578e1f0fb4d6dbc
lorry <vaiciulenaitel9@gmail.com>
2026-05-05 14:01:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const { startBot } = require("./bot");
 startBot();
 
 if (process.env.ENABLE_WEB === "true") {
-  try {
-    const { startWeb } = require("./web");
-    startWeb();
-  } catch (error) {
-    console.error("Web dashboard failed to start:", error);
-  }
+  const { startWeb } = require("./web");
+  startWeb();
 }
